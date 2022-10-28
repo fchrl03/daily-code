fce121b52a116c894bba5702f28b952561c9c2a8
Fachrul <fachrulfatahillah@gmail.com>
2022-10-28 15:59:37 +0700

    Initial Commit

--- nodejs/index.js
+++ nodejs/index.js
@@ -44,6 +44,80 @@ const onRequest = (req, res) => {
       res.end(loginIndex);
 
       return;
+
+    case '/users':
+      const users = [
+        {
+          id: 1,
+          email: 'example@mail.com',
+          name: 'example',
+          password: 'example',
+        },
+        {
+          id: 2,
+          email: 'example1@mail.com',
+          name: 'example1',
+          password: 'example1',
+        },
+      ];
+
+      const usersJSON = JSON.stringify(users);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(200);
+      res.end(usersJSON);
+
+      return;
+
+    case '/products':
+      const product = [
+        {
+          id: 1,
+          name: 'Apel',
+          price: '7000',
+          stock: '20',
+        },
+        {
+          id: 2,
+          name: 'Mangga',
+          price: '7000',
+          stock: '30',
+        },
+      ];
+
+      const productJSON = JSON.stringify(product);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(200);
+      res.end(productJSON);
+
+      return;
+
+    case '/transactions':
+      const transaction = [
+        {
+          id: 1,
+          user_id: 1,
+          product_id: 1,
+          total_price: 35000,
+          total_product: 5,
+        },
+        {
+          id: 2,
+          user_id: 2,
+          product_id: 1,
+          total_price: 35000,
+          total_product: 5,
+        },
+      ];
+
+      const transactionJSON = JSON.stringify(transaction);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(200);
+      res.end(transactionJSON);
+
+      return;
     default:
       res.setHeader('Content-Type', 'text/html');
       res.writeHead(404);
