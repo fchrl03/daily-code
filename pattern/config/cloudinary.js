bf136392d9decd1a35d8e11678334bc9804d315d
Fachrul <fachrulfatahillah@gmail.com>
2022-11-22 18:41:31 +0700

    initial commit

---
+++ pattern/config/cloudinary.js
@@ -0,0 +1,11 @@
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY_CLOUD,
+  api_secret: process.env.API_SECRET_CLOUD,
+  secure: true,
+});
+
+module.exports = cloudinary;
